35d1e85b57b7a46ddc09e31937a1ea6e3f7f3cfc
AlexandroJoeClaudio <91466410+AlexandroJoeClaudio@users.noreply.github.com>
2021-10-13 13:23:52 +0700

    Added number 4

--- # Number 4.py
+++ # Number 4.py
@@ -1,5 +1,28 @@
 # Number 4
-a = float(input("Enter the number of packages purchased"))
+a = float(input("Enter the number of packages purchased: "))
+b = a * 99
+c = b * (10/100)
+d = b * (20/100)
+e = b * (30/100)
+f = b + (40/100)
 
-if a >= 10 or <= 19:
-    print ("Discount Amount @ 10%:", )
+g = b - c
+h = b - d
+i = b - e
+j = b - f
+
+if a >= 10 and a <= 19:
+    print("Discount Amount @ 10%: $ ", format(c, '.2f'))
+    print("Total Amount: $", format(g,'.2f') )
+elif a >= 20 and a <= 49:
+    print("Discount Amount @ 20%: $ ", format(d,'.2f'))
+    print("Total Amount: $", format(h,'.2f') )
+elif a >= 50 and a <= 99:
+    print("Discount Amount @ 30%: $ ", format(e,'.2f'))
+    print ("Total Amount: $", format(i,'.2f'))
+elif a >= 100:
+    print("Discount Amount @ 40%: $ ", format(f,'.2f'))
+    print("Total Amount: $", format(j,'.2f'))
+else:
+    print("Discount Amount @ 0%: $ 0.00")
+    print("Total Amount: $", format(b,'.2f'))
